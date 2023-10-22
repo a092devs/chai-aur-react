58be9cf56a9529e57014398ae8f2d49f00e7bad6
a092devs <arsalanakhtar0@gmail.com>
2023-10-22 12:14:28 +0530

    created 2 projects using native and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur React | Arsalan Akhtar - learnt how to setup react project through native and vite way</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Chai aur React with Vite | Arsaalan Akhtar</h1>
+  )
+}
+
+export default App
