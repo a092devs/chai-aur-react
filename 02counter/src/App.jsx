7c640d5576934f20bfd43e021122e2e24185ba61
a092devs <arsalanakhtar0@gmail.com>
2023-10-23 13:47:06 +0530

    added counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(5)
+
+  // let counter = 0
+
+  const addValue= () => {
+    if (counter < 10) {
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Increment Value: {counter}</button>
+      <br />
+      <button onClick={removeValue}>Decrement Value: {counter}</button>
+      <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
