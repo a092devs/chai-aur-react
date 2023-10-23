d32191a1dc9c68c209e21eedcdfd6011ae478dc8
a092devs <arsalanakhtar0@gmail.com>
2023-10-23 12:57:26 +0530

    creating own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./chai";
 
 function App() {
+  const username = 'a092devs'
   return (
     // fragment <> empty tags
     <>
       <Chai />
-      <h3>chai aur react</h3>
+      <h3>chai aur react ny {username}</h3>
       <p>test paragraph</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App | a092devs</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank',
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const anotherUser = ' by a092devs'
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click here to visit google',
+    anotherElement
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    // MyApp()
+    // <MyApp />
+    // ReactElement
+    anotherElement
+    // reactElement
+    // <App />
 )
